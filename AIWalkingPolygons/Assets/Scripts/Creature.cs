5869d65dd410361ef909dde6258ceb7090e51dfa
microlm <laura.mo@gmail.com>
2016-02-23 00:41:42 -0500

    Initial Project
    - Script that creates polygons based on points
    - Script that joins the polygons into one creature
    - Can apply forces to each polygon individually

---
+++ AIWalkingPolygons/Assets/Scripts/Creature.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Creature : MonoBehaviour 
+{
+	List<GameObject> segments;
+	List<HingeJoint2D> joints;
+
+	public float force;
+	// Use this for initialization
+	void Start () 
+	{
+		segments = new List<GameObject> ();
+		joints = new List<HingeJoint2D> ();
+		foreach(Transform child in transform)
+		{
+			segments.Add (child.gameObject);
+		}
+
+		LinkSegments (segments);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkMove ();
+	}
+
+	void LinkSegments(List<GameObject> segments)
+	{
+		for(int i = 0; i < segments.Count - 1; i++)
+		{
+			HingeJoint2D joint = segments[i].AddComponent<HingeJoint2D>();
+			PolygonMesh mesh1 = segments[i].GetComponent<PolygonMesh>();
+			joint.anchor = mesh1.getRightMostVertice();
+
+			joint.connectedBody = segments[i+1].GetComponent<Rigidbody2D>();
+			PolygonMesh mesh2 = segments[i+1].GetComponent<PolygonMesh>();
+			joint.connectedAnchor = mesh2.getLeftMostVertice();
+
+			joint.enableCollision = true;
+			joints.Add(joint);
+		}
+	}
+
+	void checkMove()
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha0))
+		{
+			segments[0].GetComponent<Rigidbody2D>().AddForce(force * Vector2.up, ForceMode2D.Impulse);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+			segments[1].GetComponent<Rigidbody2D>().AddForce(force * Vector2.up, ForceMode2D.Impulse);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+			segments[2].GetComponent<Rigidbody2D>().AddForce(force * Vector2.up, ForceMode2D.Impulse);
+		}
+	}
+}
---
+++ AIWalkingPolygons/Assets/Scripts/PolygonMesh.cs
@@ -0,0 +1,113 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PolygonMesh : MonoBehaviour 
+{
+	public List<Vector3> points;
+	public Material material;
+
+	public Mesh mesh;
+	PolygonCollider2D polyCol;
+
+	// Use this for initialization
+	void Awake () {
+		CreateMesh ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CreateMesh()
+	{
+		mesh = new Mesh ();
+		mesh.vertices = points.ToArray ();
+
+		List<Vector2> vector2Points = new List<Vector2>();
+		
+		for(int i = 0; i < points.Count; i++)
+		{
+			vector2Points.Add(new Vector2(points[i].x, points[i].y));
+		}
+
+		mesh.uv = vector2Points.ToArray();
+		mesh.triangles = Triangulate (points.Count).ToArray();
+
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+
+		MeshRenderer renderer = gameObject.AddComponent<MeshRenderer> ();
+		MeshFilter filter = gameObject.AddComponent<MeshFilter> ();
+		filter.mesh = mesh;
+		renderer.material = material;
+		polyCol = gameObject.AddComponent<PolygonCollider2D>();
+
+		polyCol.points = vector2Points.ToArray ();
+
+	}
+
+	/*-----------------------------------------------------
+	 * Creates 2 triangles out of a group of 4 points
+	 * vertices go top bottom top bottom, so imagine:
+	 * 
+	 *       0____2           
+	 *       |\   |
+	 *       | \T2|
+	 *       |  \ |
+	 *       |T1 \|
+	 *       1____3   
+	 * ---------------------------------------------------*/
+	
+	public List<int> Triangulate (int count)
+	{
+		List<int> indices = new List<int> ();
+
+		int c1 = 0;
+		int c2 = 1;
+		int c3 = 2;
+		for(int i = 0 ; i < count-2; i++)
+		{
+			indices.Add(c1);
+			indices.Add (c2);
+			indices.Add (c3);
+
+			c2++;
+			c3++;
+		}
+
+		return indices;
+	}
+
+	public Vector2 getRightMostVertice()
+	{
+		int index = 0;
+
+		for(int i = 1; i < mesh.vertices.Length; i++)
+		{
+			if(mesh.vertices[index].x < mesh.vertices[i].x)
+			{
+				index = i;
+			}
+		}
+
+		return new Vector2(mesh.vertices[index].x, mesh.vertices[index].y);
+	}
+
+	public Vector2 getLeftMostVertice()
+	{
+		int index = 0;
+		
+		for(int i = 1; i < mesh.vertices.Length; i++)
+		{
+			if(mesh.vertices[index].x > mesh.vertices[i].x)
+			{
+				index = i;
+			}
+		}
+		
+		return new Vector2(mesh.vertices[index].x, mesh.vertices[index].y);
+	}
+}
