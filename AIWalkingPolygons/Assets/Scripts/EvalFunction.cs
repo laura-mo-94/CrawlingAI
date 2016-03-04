f0e3429d620f604e1918084600e529d10c34a488
Aurelis-Loire <johanna.oberto@gmail.com>
2016-03-04 17:04:21 -0500

    Evaluation Function
    Function for evaluating the fitness of the individuals

---
+++ AIWalkingPolygons/Assets/Scripts/EvalFunction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvalFunction : MonoBehaviour {
+
+	int evalFunction (Creature c) {
+		int distance = 0;
+		distance = c.getStart() - c.getEnd();
+		return distance;
+	}
+}
