3c4d7913fa495b7a84619cd1f58952853a297925
Aurelis-Loire <johanna.oberto@gmail.com>
2016-03-12 10:18:14 -0500

    Added Serializer V1
    Serialize takes in sequence,
    Deserialize returns sequence

---
+++ AIWalkingPolygons/Assets/Scripts/Serializer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Serializer : MonoBehaviour {
+
+	public void Serialize(List<MovementNode> sequence)
+	{
+		using (var stream = File.Create("seq.xml")) {
+			var serializer = new XmlSerializer(typeof(List<MovementNode>));
+			serializer.Serialize(stream, sequence);
+		}
+	}
+
+	public List<MovementNode> Deserialize()
+	{
+		// IF DOESN'T WORK, change to the path of "seq.xml" on your computer
+		string path = "seq.xml";
+		List<MovementNode> sequence = new List<MovementNode>();
+		XmlSerializer serializer = new XmlSerializer(typeof(List<MovementNode>));
+		StreamReader reader = new StreamReader(path);
+
+		sequence = (List<MovementNode>)serializer.Deserialize(reader);
+		reader.Close();
+
+		return sequence;
+	}
+}
